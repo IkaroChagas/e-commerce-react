c5eb6764d34e3cf4e232286edcc1db0bc7741d36
Ikaro Chagas <ikarochagas.ig@gmail.com>
2022-09-26 20:56:26 -0300

    Update cart.ts

--- src/reducers/cart.ts
+++ src/reducers/cart.ts
@@ -20,3 +20,37 @@ export const addProductToCart = (product: { id: string; }, state: { cart: any; }
     
       return { ...state, cart: updatedCart };
     };
+
+    
+
+    const removeProductFromCart = (productId, state) => {
+      console.log("remove product: " + productId);
+      const updatedCart = [...state.cart];
+      const updatedItemIndex = updatedCart.findIndex(item => item.id === productId);
+    
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity--;
+      if (updatedItem.quantity <= 0) {
+        updatedCart.splice(updatedItemIndex, 1);
+      } else {
+        updatedCart[updatedItemIndex] = updatedItem;
+      }
+    
+      return { ...state, cart: updatedCart };
+    };
+    
+    export const shopReducer = (state, action) => {
+      switch (action.type) {
+        case ADD_PRODUCT:
+          return addProductToCart(action.product, state);
+    
+        case REMOVE_PRODUCT:
+          return removeProductFromCart(action.productId, state);
+    
+        default:
+          return state;
+      }
+    };
+    
