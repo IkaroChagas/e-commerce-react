831b3329195abde7356ab4da98da954d8dff92ca
Ikaro <ikaro.chagas1@gamil.com>
2022-10-05 14:46:17 -0300

    updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,12 @@
 import * as C from './styles'
 import { productURL } from '../../Api/Api'
-import { useEffect, useState } from 'react'
-import { ProductsDesc } from '../../types/Products'
 import { useDispatch } from 'react-redux'
 import { addProduct } from '../../redux/slices/productsSlice'
-// import { useParams } from 'react-router-dom'
+
 
 export const Button = ({ productId }: { productId: string }) => {
     const dispatch = useDispatch()
-    // const params = useParams()
 
-    // const [cartItem, setCartItem] = useState<ProductsDesc[]>([])
-
-    // useEffect(() => {
-    //     if (params.id) {
-    //         handleAddProduct(params.id)
-    //     }
-    // }, [])
 
     const handleAddProduct = async (productId: string) => {
         const product = await productURL.getCart(productId)
