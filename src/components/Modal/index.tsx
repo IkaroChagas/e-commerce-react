a1b287acd7552450e96bf637e0c3ff5e379a918f
Ikaro Chagas <ikarochagas.ig@gmail.com>
2022-10-07 21:13:48 -0300

    data added

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { useCart } from 'react-use-cart';
 import * as C from './styles'
 import cartImage from '../../images/cart.png'
+import data from '../../data';
+
 
 
 export const MainModal = () => {
-
+   
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
     function handleOpenModal() {
@@ -14,8 +17,48 @@ export const MainModal = () => {
 
     function afterOpenModal() {
 
+  const { addItem } = useCart();
 
-    }
+  const products = [
+    {
+       
+    },
+    {
+      id: 2,
+      name: "Nordli",
+      price: 16500,
+      quantity: 5
+    },
+    {
+      id: 3,
+      name: "Kullen",
+      price: 4500,
+      quantity: 1
+    },
+  ];
+
+  return (
+    <div>
+      {products.map((p) => (
+        <div key={p.id}>
+          <button onClick={() => addItem(p)}>Add to cart</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+        const {
+          isEmpty,
+          totalUniqueItems,
+          items,
+          updateItemQuantity,
+          removeItem,
+        } = useCart();
+      
+        if (isEmpty) return <p>Your cart is empty</p>;
+
+    // termina aqui
 
     function handleCloseModal() {
         setModalIsOpen(false);
@@ -40,9 +83,12 @@ export const MainModal = () => {
                 style={customStyles}
                 contentLabel="Example Modal"
             >
+                
                 <button onClick={handleCloseModal}>Close Modal</button>
             </Modal>
         </C.Container>
 
     );
 }
+
+
